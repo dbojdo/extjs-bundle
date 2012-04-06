9d01ee6cab58c58c4ce97f53bc4cae3b3ea9c4de
dbojdo <dbojdo@192.168.111.78>
2012-04-06 13:33:11 +0200

    [new] begining of bundle development

---
+++ Command/Install.php
@@ -0,0 +1,13 @@
+<?php
+namespace Webit\Bundle\ExtJsBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class Install extends ContainerAwareCommand {
+  
+}
+?>
---
+++ WebitExtjsBundle.php
@@ -0,0 +1,8 @@
+<?php
+namespace Webit\Bundle\ExtJsBundle;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class WebitExtJsBundle extends Bundle {
+  
+}
+?>
