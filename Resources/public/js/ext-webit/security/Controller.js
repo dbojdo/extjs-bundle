97f3291abbced0df3d5c8ef88cc00a9c24642d9b
dbojdo <daniel.bojdo@web-it.eu>
2012-11-28 16:03:28 +0100

    [fix] security controller

--- Resources/public/js/ext-webit/security/Controller.js
+++ Resources/public/js/ext-webit/security/Controller.js
@@ -6,11 +6,32 @@ Ext.define('Webit.security.Controller',{
 		this.control({
 			'container' : {
 				beforeadd: function(container, component) {
-					if(Ext.isFunction(component.isAllowed)) {
-						return component.isAllowed(SecurityContext['user']);
-					}
+					return this.isAllowed(SecurityContext['user'], component);
 				}
 			}
 		});
+	},
+	isAllowed: function(user, component) {
+		if(Ext.isFunction(component.isAllowed)) {
+			return component.isAllowed(user);
+		}
+		
+		if(Ext.isArray(component.allowedRoles)) {
+			if(Ext.Array.contains('*')) {
+				return true;
+			}
+			
+			isAllowed = false;
+			Ext.Array.each(component.allowedRoles,function(role) {
+				if(user.hasRole(role)) {
+					isAllowed = true;
+					return false;
+				}
+			});
+			
+			return isAllowed;
+		}
+		
+		return true;
 	}
 });
