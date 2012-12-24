953b9768984bd9e83c6cf98e379533c345cd12be
dbojdo <daniel.bojdo@web-it.eu>
2012-12-24 15:03:26 +0100

    [upd] setXtype (chart)

--- Chart/Chart.php
+++ Chart/Chart.php
@@ -1,6 +1,5 @@
 <?php
 namespace Webit\Bundle\ExtJsBundle\Chart;
-
 use Webit\Bundle\ExtJsBundle\Chart\Axis\Axis;
 use Webit\Bundle\ExtJsBundle\Chart\Series\Series;
 use JMS\Serializer\Annotation as JMS;
@@ -36,7 +35,7 @@ class Chart {
 	 * @JMS\Groups({"chartInfo"})
 	 */
 	protected $xtype = 'chart';
-	
+
 	/**
 	 * 
 	 * @var string
@@ -45,27 +44,27 @@ class Chart {
 	 * @JMS\Groups({"chartInfo"})
 	 */
 	protected $itemId;
-	
+
 	/**
-   * @JMS\Type("boolean")
+	 * @JMS\Type("boolean")
 	 * @JMS\SerializedName("animate")
 	 * @JMS\Groups({"chartInfo"})
 	 */
 	protected $animate;
-	
+
 	/**
-   * @JMS\Type("array")
+	 * @JMS\Type("array")
 	 * @JMS\SerializedName("axes")
 	 * @JMS\Groups({"chartInfo"})
 	 */
 	protected $axes = array();
-	
+
 	protected $background;
-	
+
 	protected $gradients = array();
-	
+
 	protected $insetPadding = 10;
-	
+
 	/**
 	 * @var Legend
 	 * @JMS\Type("Webit\Bundle\ExtJsBundle\Chart\Legend")
@@ -73,7 +72,7 @@ class Chart {
 	 * @JMS\Groups({"chartInfo"})
 	 */
 	protected $legend;
-	
+
 	/**
 	 * 
 	 * @var array
@@ -82,7 +81,7 @@ class Chart {
 	 * @JMS\Groups({"chartInfo"})
 	 */
 	protected $series = array();
-	
+
 	/**
 	 * 
 	 * @var array
@@ -91,104 +90,112 @@ class Chart {
 	 * @JMS\Groups({"chartInfo"})
 	 */
 	protected $store = array();
-	
+
 	/**
 	 * @JMS\Type("string")
 	 * @JMS\SerializedName("theme")
 	 * @JMS\Groups({"chartInfo"})
 	 */
 	protected $theme = self::THEME_BASE;
-	
+
 	public function __construct() {
 		$this->legend = new Legend();
 	}
-	
+
 	public function setItemId($itemId) {
 		$this->itemId = $itemId;
 	}
-	
+
 	public function getItemId() {
 		return $this->itemId;
 	}
-	
-	public function getAnimate(){
+
+	public function getAnimate() {
 		return $this->animate;
 	}
-	
-	public function setAnimate($animate){
+
+	public function setAnimate($animate) {
 		$this->animate = $animate;
 	}
-	
-	public function getAxes(){
+
+	public function getAxes() {
 		return $this->axes;
 	}
-	
-	public function setAxes(array $axes){
+
+	public function setAxes(array $axes) {
 		$this->axes = $axes;
 	}
-	
+
 	public function addAxe(Axis $axe) {
 		$this->axes[] = $axe;
 	}
-	
-	public function getBackground(){
+
+	public function getBackground() {
 		return $this->background;
 	}
-	
-	public function setBackground($background){
+
+	public function setBackground($background) {
 		$this->background = $background;
 	}
-	
-	public function getGradients(){
+
+	public function getGradients() {
 		return $this->gradients;
 	}
-	
-	public function setGradients($gradients){
+
+	public function setGradients($gradients) {
 		$this->gradients = $gradients;
 	}
-	
-	public function getInsetPadding(){
+
+	public function getInsetPadding() {
 		return $this->insetPadding;
 	}
-	
-	public function setInsetPadding($insetPadding){
+
+	public function setInsetPadding($insetPadding) {
 		$this->insetPadding = $insetPadding;
 	}
-	
-	public function getLegend(){
+
+	public function getLegend() {
 		return $this->legend;
 	}
-	
-	public function setLegend(Legend $legend){
+
+	public function setLegend(Legend $legend) {
 		$this->legend = $legend;
 	}
-	
-	public function getSeries(){
+
+	public function getSeries() {
 		return $this->series;
 	}
-	
-	public function setSeries(array $series){
+
+	public function setSeries(array $series) {
 		$this->series = $series;
 	}
-	
+
 	public function addSeries(Series $series) {
 		$this->series[] = $series;
 	}
-	
-	public function getStore(){
+
+	public function getStore() {
 		return $this->store;
 	}
-	
-	public function setStore($store){
+
+	public function setStore($store) {
 		$this->store = $store;
 	}
-	
-	public function getTheme(){
+
+	public function getTheme() {
 		return $this->theme;
 	}
-	
-	public function setTheme($theme){
+
+	public function setTheme($theme) {
 		$this->theme = $theme;
+	}
+
+	public function getXtype() {
+		return $this->xtype;
+	}
+
+	public function setXtype($xtype) {
+		$this->xtype = $xtype;
 	}
 }
 ?>
