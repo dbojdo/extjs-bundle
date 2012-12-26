58a5b56feb1bbfb0bcc14440276a96bef4255e5d
dbojdo <daniel.bojdo@web-it.eu>
2012-12-26 20:29:58 +0100

    [fix] clearable field

--- Resources/public/js/ext-webit/form/field/ClearableField.js
+++ Resources/public/js/ext-webit/form/field/ClearableField.js
@@ -10,19 +10,20 @@ Ext.define('Webit.form.field.ClearableField',{
 	},
 	initComponent: function() {
 		var items = this.items || [];
-		items.push({
-			xtype: 'button',
-			text: 'x',
-			width: 21,
-			flex: null,
-			disabled: true,
-			tooltip: 'Wyczyść',
-			handler: function(btn) {
-				btn.prev('field').clearValue();
-				btn.disable();
-			}
-		});
-		
+		if(items.length > 0 && items[items.length - 1].xtype != 'button') {
+			items.push({
+				xtype: 'button',
+				text: 'x',
+				width: 21,
+				flex: null,
+				disabled: true,
+				tooltip: 'Wyczyść',
+				handler: function(btn) {
+					btn.prev('field').clearValue();
+					btn.disable();
+				}
+			});
+		}
 		this.callParent();
 		
 		this.down('field').on('change',function(field) {
