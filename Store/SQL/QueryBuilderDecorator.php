5bac480219ddb80d4be2df8171618709a0777a59
dbojdo <daniel.bojdo@web-it.eu>
2013-06-25 15:23:33 +0200

    [fix]

--- Store/SQL/QueryBuilderDecorator.php
+++ Store/SQL/QueryBuilderDecorator.php
@@ -1,15 +1,12 @@
 <?php
-namespace Webit\Bundle\ExtJsBundle\Store\SQL;
+namespace Webit\Bundle\ExtJsBundle\Store\QOM;
 
-use Webit\Bundle\ExtJsBundle\Store\Filter\FilterParams;
-
-use Webit\Bundle\ExtJsBundle\Store\Filter\FilterParamsInterface;
-
+use PHPCR\Query\QOM\QueryObjectModelConstantsInterface as Constants;
 use Webit\Bundle\ExtJsBundle\Store\Filter\FilterInterface;
-
-use Webit\Bundle\ExtJsBundle\Store\Sorter\SorterCollectionInterface;
-use Webit\Bundle\ExtJsBundle\Store\Filter\FilterCollectionInterface;
-use Doctrine\DBAL\Query\QueryBuilder;
+use Webit\Bundle\ExtJsBundle\Store\Sorter\SorterCollection;
+use Webit\Bundle\ExtJsBundle\Store\Filter\FilterCollection;
+use PHPCR\Util\QOM\QueryBuilder;
+use PHPCR\Query\QOM\QueryObjectModelFactoryInterface;
 
 class QueryBuilderDecorator {
 	/**
@@ -18,14 +15,24 @@ class QueryBuilderDecorator {
 	 */
 	protected $qb;
 	
+	/**
+   * @var QueryObjectModelFactoryInterface
+	 */
+	protected $qf;
+	
+	/**
+	 * 
+	 * @var array
+	 */
 	protected $propertyMap;
 	
 	/**
 	 * 
 	 * @param QueryBuilder $qb
 	 */
-	public function __construct(QueryBuilder $qb, $propertyMap = array()) {
+	public function __construct(QueryBuilder $qb, array $propertyMap = array()) {
 		$this->qb = $qb;
+		$this->qf = $qb->getQOMFactory();
 		$this->propertyMap = $propertyMap;
 	}
 	
@@ -33,7 +40,7 @@ class QueryBuilderDecorator {
 	 * 
 	 * @param FilterCollection $filterCollection
 	 */
-	public function applyFilters(FilterCollectionInterface $filterCollection) {
+	public function applyFilters(FilterCollection $filterCollection) {
 		foreach($filterCollection as $filter) {
 			$property = $filter->getProperty();
 			$property = $this->getQueryProperty($property);
@@ -54,233 +61,113 @@ class QueryBuilderDecorator {
 				case FilterInterface::TYPE_LIST:
 					$this->applyListFilter($property, $filter);
 				break;
+				case FilterInterface::TYPE_NODE:
+					$this->applyNodeFilter($property, $filter);
+				break;
 			}
 		}
 		
 		return $this;
 	}
-	
-	protected function applyDateFilter($property, FilterInterface $filter) {
+
+	protected function applyNodeFilter($arFields, FilterInterface $filter) {
 		$qb = $this->qb;
-		$property = array_shift($property);
+		$qf = $this->qf;
 		
-		$paramName = 'date_' . substr(md5(serialize($filter) . microtime()),0,10);
-		$value = new \DateTime($filter->getValue());
-		if($filter->getType() == FilterInterface::TYPE_DATE) {
-			$value->setTime(0, 0, 0);
+		$constraint = null;
+		foreach($arFields as $qField) {
+			switch($filter->getComparision()) {
+				case FilterInterface::COMPARISION_CHILD:
+					$c = $qf->childNode($filter->getValue(),$qField->getAlias());
+				break;
+				case FilterInterface::COMPARISION_DESCENDANT:
+					$c = $qf->descendantNode($filter->getValue(),$qField->getAlias());
+				break;
+				case FilterInterface::COMPARISION_CHILD_OR_EQUAL:
+					$c = $qf->orConstraint($qf->childNode($filter->getValue(),$qField->getAlias()),
+						$qf->sameNode($filter->getValue(),$qField->getAlias()));
+				break;
+				case FilterInterface::COMPARISION_DESCENDANT_OR_EQUAL:
+					$c = $qf->orConstraint($qf->descendantNode($filter->getValue(),$qField->getAlias()),
+					$qf->sameNode($filter->getValue(),$qField->getAlias()));
+				break;
+				default:
+					$c = $qf->sameNode($filter->getValue(),$qField->getAlias());
+			}
+			
+			if($constraint) {
+				$constraint = $qf->orConstraint($constraint,$c);
+			} else {
+				$constraint = $c;
+			}
 		}
 		
-		switch($filter->getComparision()) {
-			case FilterInterface::COMPARISION_GREATER:
-				$qb->andWhere($qb->expr()->gt($property,(':'.$paramName)));
-				$qb->setParameter($paramName, $value,'datetime');
-			break;
-			case FilterInterface::COMPARISION_LESS:
-				$qb->andWhere($qb->expr()->lt($property,(':'.$paramName)));
-				$qb->setParameter($paramName, $value,'datetime');
-			break;
-			case FilterInterface::COMPARISION_LESS_OR_EQUAL:
-				if($filter->getType() == FilterInterface::TYPE_DATE) {
-					$value->add(new \DateInterval('P1D'));
-					$qb->andWhere($qb->expr()->lt($property,(':'.$paramName)));
-					$qb->setParameter($paramName, $value,'datetime');
-				} else {
-					$qb->andWhere($qb->expr()->lte($property,(':'.$paramName)));
-					$qb->setParameter($paramName, $value,'datetime');
-				}
-			break;
-			case FilterInterface::COMPARISION_GREATER_OR_EQUAL:
-				$qb->andWhere($qb->expr()->gte($property,(':'.$paramName)));
-				$qb->setParameter($paramName, $value,'datetime');
-			break;
-			default:
-				// FilterInterface::COMPARISION_EQUAL:
-				if($filter->getType() == FilterInterface::TYPE_DATE) {
-					$qb->andWhere($qb->expr()->gte($property,(':'.$paramName)));
-					$qb->setParameter($paramName, $value,'datetime');
-					
-					$valueTo = clone($value);
-					$valueTo->add(new \DateInterval('P1D'));
-					$valueTo = $valueTo->format('Y-m-d H:i:s');
-					$qb->andWhere($qb->expr()->lt($property,(':'.$paramName.'_2')));
-					$qb->setParameter(($paramName.'_2'), $valueTo,'datetime');
-				} else {
-					$qb->andWhere($qb->expr()->eq($property,(':'.$paramName)));
-					$qb->setParameter($paramName, $value,'datetime');
-				}
+		if($constraint) {
+			$qb->andWhere($constraint);
 		}
 	}
 	
-	protected function applyStringFilter($property, FilterInterface $filter) {
+	protected function applyChildFilter($arFields, FilterInterface $filter) {
 		$qb = $this->qb;
-		$value = (string)$filter->getValue();
+		$qf = $this->qf;
+	
+		$constraint = null;
+		foreach($arFields as $qField) {
+			$c = $qf->childNode($filter->getValue(),$f->getAlias());
+			if($constraint) {
+				$constraint = $qf->orConstraint($constraint,$c);
+			} else {
+				$constraint = $c;
+			}
+		}
+	
+		if($constraint) {
+			$qb->andWhere($constraint);
+		}
+	}
+	
+	public function applyStringFilter($arFields, FilterInterface $filter) {
+		$value = $filter->getValue();
 		if(empty($value)) {
 			return;
 		}
 		
-		$value = $this->getStringValueExpression($filter->getParams(), $value);
-		
-		$cs = $filter->getParams()->getCaseSensitive();
-		$arCond = array();
-		foreach($property as $f) {
-			$cond = $qb->expr()->like(($cs ? $f : $qb->expr()->lower($f)),$value);
-			$arCond[] = $filter->getParams()->getNegation() ? $qb->expr()->not($cond) : $cond;
-		}
-		
-		if(count($arCond) > 0) {
-			$this->qb->andWhere(call_user_func_array(array($qb->expr(),'orx'), $arCond));
-		}
-	}
-	
-	protected function applyNumericFilter($property, FilterInterface $filter) {
 		$qb = $this->qb;
-		$value = (float)$filter->getValue();
-		$arCond = array();
-		foreach($property as $f) {
-			switch($filter->getComparision()) {
-				case FilterInterface::COMPARISION_GREATER:
-					$arCond[] = $qb->expr()->gt($f,$value);
-					break;
-				case FilterInterface::COMPARISION_LESS:
-					$arCond[] = $qb->expr()->lt($f,$value);
-					break;
-				case FilterInterface::COMPARISION_LESS_OR_EQUAL:
-					$arCond[] = $qb->expr()->lte($f,$value);
-					break;
-				case FilterInterface::COMPARISION_GREATER_OR_EQUAL:
-					$arCond[] = $qb->expr()->gte($f,$value);
-					break;
-				case FilterInterface::COMPARISION_NOT:
-					$arCond[] = $qb->expr()->neq($f,$value);
-					break;
-				default:
-					//FilterInterface::COMPARISION_EQUAL:
-					$arCond[] = $qb->expr()->eq($f,$value);
+		$qf = $this->qf;
+		
+		$constraint = null;
+		foreach($arFields as $qField) {
+			$c = $qf->comparison($qf->propertyValue($qField->getName(),$qField->getAlias()), Constants::JCR_OPERATOR_LIKE, $qf->literal($filter->getValue()));
+			if($constraint) {
+				$constraint = $qf->orConstraint($constraint,$c);
+			} else {
+				$constraint = $c;
 			}
 		}
-	
-		if(count($arCond) > 0) {
-			$this->qb->andWhere(call_user_func_array(array($qb->expr(),'orx'), $arCond));
-		}
-	}
-	
-	protected function applyBooleanFilter($property, FilterInterface $filter) {
-		$qb = $this->qb;
-		$value = (boolean)$filter->getValue();
-		$arCond = array();
-		foreach($property as $f) {
-			$arCond[] = $qb->expr()->eq($f,$qb->expr()->literal($value));
-		}
-	
-		if(count($arCond) > 0) {
-			$this->qb->andWhere(call_user_func_array(array($qb->expr(),'orx'), $arCond));
-		}
-	}
-	
-	protected function applyListFilter($property, FilterInterface $filter) {
-		$qb = $this->qb;
-		$arValue = explode(',',$filter->getValue());
 		
-		$arCond = array();
-		foreach($property as $f) {
-			$arCond[] = $qb->expr()->in($property,$arValue);
+		if($constraint) {
+			$qb->andWhere($constraint);
 		}
-		
-		$qb->andWhere($qb->expr()->orx($arCond));
 	}
 	
 	/**
 	 * 
 	 * @param SorterCollection $sorterCollection
 	 */
-	public function applySorters(SorterCollectionInterface $sorterCollection) {
-		foreach($sorterCollection as $sorter) {
-			$arFields = $this->getQueryProperty($sorter->getProperty());
-			foreach($arFields as $f) {
-				$this->qb->addOrderBy($f,$sorter->getDirection());
-			}
-		}
-		
-		return $this;
-	}
-	
-	public function applySearching($query,array $fields, FilterParamsInterface $filterParams = null) {
-		$filterParams = $filterParams ?: new FilterParams(array('case_sensitive'=>false,'like_wildcard'=>FilterParamsInterface::LIKE_WILDCARD_RIGHT));
-		
-		$query = $this->getStringValueExpression($filterParams, $query);
-		$cs = $filterParams->getCaseSensitive();
+	public function applySorters(SorterCollection $sorterCollection) {
 		$qb = $this->qb;
-		$arCond = array();
-		foreach($fields as $field) {
-			$arField = $this->getQueryProperty($field);
-			// FIXME: tylko po polach typu string
-			// FIXME: możliwość ustalenia like %saf% lub %dfssa lub dsfd%
-			// FIXME: możliwość ustalenia dodatkowych filtrów (lowercase, usuwanie białych znaków, przecinków itd)
-			foreach($arField as $f) {
-				$cond = $qb->expr()->like(($cs ? $f : $qb->expr()->lower($f)),$query);
-				$arCond[] = $filterParams->getNegation() ? $qb->expr()->not($cond) : $cond;
+		$qf = $this->qf;
+
+		foreach($sorterCollection as $sorter) {
+			$property = $this->getQueryProperty($sorter->getProperty());
+			foreach($property as $queryField) {
+				$qb->addOrderBy($qf->propertyValue($queryField->getName(),$queryField->getAlias()),$sorter->getDirection());
 			}
 		}
 		
-		if(count($arCond) > 0) {
-			$this->qb->andWhere(call_user_func_array(array($qb->expr(),'orx'), $arCond));
-		}
-	
 		return $this;
 	}
-	
-	private function getQueryProperty($property) {
-		$name = (array)$this->underscoreToCamelCase($property);
-		$alias = $this->qb->getRootAlias();
 
-		if(key_exists($property,$this->propertyMap)) {
-			$arProperty = $this->propertyMap[$property];
-			$alias = isset($arProperty['alias']) ? $arProperty['alias'] : $alias;
-			$name = isset($arProperty['name']) ? (array)$arProperty['name'] : $name;
-		}
-		
-		$arProperties = array();
-		foreach($name as $n) {
-			$a = $alias;
-			if(is_array($n)) {
-				$a = key_exists('alias',$n) ? $n['alias'] : $alias;
-				$n = key_exists('name',$n) ? $n['name'] : array_shift($n);
-			}
-			$arProperties[] = $alias ? ($a. '.'.$n) : $n; 
-		}
-		
-		return $arProperties;
-	}
-	
-	private function getStringValueExpression(FilterParamsInterface $filterParams, $value) {
-		$cs = $filterParams->getCaseSensitive();
-		$wc = $filterParams->getLikeWildcard();
-		switch($wc) {
-			case FilterParamsInterface::LIKE_WILDCARD_LEFT:
-				$value = '%'.$value;
-				break;
-			case FilterParamsInterface::LIKE_WILDCARD_RIGHT:
-				$value .= '%';
-				break;
-			case FilterParamsInterface::LIKE_WILDCARD_BOTH:
-				$value = '%'.$value . '%';
-				break;
-		}
-		$value = $cs ? $value : mb_strtolower($value);
-		
-		return $this->qb->expr()->literal($value);
-	}
-	
-	private function underscoreToCamelCase($string, $capitalizeFirstCharacter = false) {
-		$str = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
-	
-		if (!$capitalizeFirstCharacter) {
-			$str = lcfirst($str);
-		}
-	
-		return $str;
-	}
-	
 	/**
 	 * 
 	 * @param integer $limit
@@ -303,7 +190,71 @@ class QueryBuilderDecorator {
 		}
 		
 		return $this;
-	} 
+	}
+
+	public function applySearching($query,array $fields) {
+		$qb = $this->qb;
+		$qf = $this->qf;
+		
+		$constraint = null;
+		foreach($fields as $field) {
+			$arFields = $this->getQueryProperty($field);
+			
+			foreach($arFields as $qField) {
+				$c = $qf->comparison($qf->propertyValue($qField->getName(),$qField->getAlias()), Constants::JCR_OPERATOR_LIKE, $qf->literal($query.'%'));
+				if($constraint) {
+					$constraint = $qf->orConstraint($constraint,$c);
+				} else {
+					$constraint = $c;
+				}
+			}
+			// FIXME: tylko po polach typu string
+			// FIXME: możliwość ustalenia like %saf% lub %dfssa lub dsfd%
+			// FIXME: możliwość ustalenia dodatkowych filtrów (lowercase, usuwanie białych znaków, przecinków itd)
+		}
+	
+		if($constraint) {
+			$qb->andWhere($constraint);
+		}
+	
+		return $this;
+	}
+	
+	private function getQueryProperty($property) {
+		$name = (array)$this->underscoreToCamelCase($property);
+		$alias = key_exists('_rootAlias',$this->propertyMap) ? $this->propertyMap['_rootAlias'] : null;
+
+		if(key_exists($property,$this->propertyMap)) {
+			$arProperty = $this->propertyMap[$property];
+			$alias = isset($arProperty['alias']) ? $arProperty['alias'] : $alias;
+			$name = isset($arProperty['name']) ? (array)$arProperty['name'] : $name;
+		}
+		
+		$arProperties = array();
+		foreach($name as $n) {
+			$a = $alias;
+			if(is_array($n)) {
+				$a = key_exists('alias',$n) ? $n['alias'] : $alias;
+				$n = key_exists('name',$n) ? $n['name'] : array_shift($n);
+			}
+			$qProperty = new QueryField();
+			$qProperty->setAlias($a);
+			$qProperty->setName($n);
+			$arProperties[] = $qProperty; 
+		}
+		
+		return $arProperties;
+	}
+	
+	private function underscoreToCamelCase($string, $capitalizeFirstCharacter = false) {
+		$str = str_replace(' ', '', ucwords(str_replace('_', ' ', $string)));
+	
+		if (!$capitalizeFirstCharacter) {
+			$str = lcfirst($str);
+		}
+	
+		return $str;
+	}
 	
 	/**
 	 * 
